cba21dbbd73b45fd97575761d4a5da67fcff063e
hossam-elsheikh <hossam.m.elsheikh@gmail.com>
2024-06-11 19:15:57 +0300

    a

--- components/ItemCard/ProductCard.tsx
+++ components/ItemCard/ProductCard.tsx
@@ -9,7 +9,7 @@ const ProductCard = ({ details }) => {
   const [translate, setTranslate] = useState("");
   const QuickAccess = () => (
     <div
-      className="absolute transition duration-300 w-full h-full flex items-center justify-center  animate-slide bg-black/25 "
+      className="absolute transition duration-300 rounded-md w-full h-full flex items-center justify-center  animate-fadein bg-black/25 "
     >
       <button className="bg-white px-5 hover:scale-105 text-sm py-2 rounded-full text-primaryColor font-semibold ">
         Quick access
@@ -19,11 +19,11 @@ const ProductCard = ({ details }) => {
   return (
     <div
       
-      className="flex border transition flex-col max-w-56 rounded-lg  shadow-xl pb-1"
+      className="flex border transition p-2 flex-col max-w-52 rounded-lg  shadow-xl pb-1"
     >
       {/* <ProductDialog /> */}
       <div onMouseOver={() => setQuickAccess(true)}
-      onMouseOut={() => setQuickAccess(false)} className="bg-slate-100 w-full overflow-hidden cursor-pointer h-56 flex relative justify-center">
+      onMouseOut={() => setQuickAccess(false)} className="bg-slate-100 w-full overflow-hidden cursor-pointer h-52  flex relative justify-center">
         <Image
           src={details.image}
           alt={details.title}
@@ -45,8 +45,9 @@ const ProductCard = ({ details }) => {
         </div>
         <div className="flex items-center justify-between">
           <Heart className="cursor-pointer hover:text-red-500 transition " />
-          <div className="flex bg-primaryColor px-6 py-2 rounded-md text-white items-center gap-2 hover:bg-secColor transition cursor-pointer">
-            <p className="text-sm">Add to cart</p>
+          <div className="flex bg-primaryColor px-2 py-2 rounded-md text-white items-center gap-2 hover:bg-secColor transition cursor-pointer">
+            <p className="text-sm font-medium hidden sm:block">Add to cart</p>
+            <p className="text-xl font-medium block sm:hidden">+</p>
             <ShoppingCart />
           </div>
         </div>
